0134a4f9f4cce2e58df0e0410d9c99055220464a
sushmitamaurya <mauryasushmita83@gmail.com>
2022-09-07 15:47:15 +0530

    new changes

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +7,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('demo')),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          padding: EdgeInsets.all(40),
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.pink[100],
+          child: Column(
+            children: [
+              Text("hello wolrd!"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
